c514324717a7c2952e29d1010c76401fae7f917d
dpopkov <pkvdenis@gmail.com>
2018-01-17 11:50:17 +0300

    week03: jee-datalayer: Sort list of employees by id (descending)

--- week03/JEE_DataLayer/src/main/java/ru/otus/dpopkov/jdbc/servlets/EmployeeServlet.java
+++ week03/JEE_DataLayer/src/main/java/ru/otus/dpopkov/jdbc/servlets/EmployeeServlet.java
@@ -26,20 +26,15 @@ public class EmployeeServlet extends HttpServlet {
         EntityTransaction transaction = em.getTransaction();
         try {
             transaction.begin();
-            Query query = em.createQuery("from Employee");
+            Query query = em.createQuery("select e from Employee as e order by e.id desc");
             List<Employee> employees = query.getResultList();
             PrintWriter writer = writeHtmlHeader(resp);
-            writer.append("<table>\r\n");
+
             for (Employee emp : employees) {
-                writer.append("<tr><td>");
-                writer.append(emp.getFullName());
-                writer.append("</td><td>\r\n");
-                writer.append(emp.getLogin());
-                writer.append("</td><td>\r\n");
-                writer.append(emp.getPosition().getName());
-                writer.append("</td></tr>\r\n");
+                writeEmployee(writer, emp);
             }
-            writer.append("</table></body></html>");
+
+            writeHtmlFooter(writer);
         } catch (Exception e) {
             transaction.rollback();
             throw new ServletException(e);
@@ -50,9 +45,26 @@ public class EmployeeServlet extends HttpServlet {
         transaction.commit();
     }
 
+    private void writeEmployee(PrintWriter writer, Employee emp) {
+        writer.append("<tr><td>");
+        writer.append(emp.getId().toString());
+        writer.append("</td><td>\r\n");
+        writer.append(emp.getFullName());
+        writer.append("</td><td>\r\n");
+        writer.append(emp.getLogin());
+        writer.append("</td><td>\r\n");
+        writer.append(emp.getPosition().getName());
+        writer.append("</td></tr>\r\n");
+    }
+
     private PrintWriter writeHtmlHeader(HttpServletResponse resp) throws IOException {
         PrintWriter writer = resp.getWriter();
         writer.append("<html><head><title>Employee Servlet</title></head><body>\r\n");
+        writer.append("<table>\r\n");
         return writer;
+    }
+
+    private void writeHtmlFooter(PrintWriter writer) {
+        writer.append("</table></body></html>");
     }
 }
