856003ff5c2419faac862f024edd6715a3d359c4
dpopkov <pkvdenis@gmail.com>
2018-01-21 20:23:47 +0300

    lect05: HW03: remove not needed private methods

--- lect03/JEE_DataLayer/src/main/java/ru/otus/dpopkov/xmljsonhw/research/HowToReadJSON.java
+++ lect03/JEE_DataLayer/src/main/java/ru/otus/dpopkov/xmljsonhw/research/HowToReadJSON.java
@@ -6,7 +6,6 @@ import ru.otus.dpopkov.jdbc.model.Employee;
 import ru.otus.dpopkov.jdbc.model.Employees;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -21,39 +20,4 @@ public class HowToReadJSON {
             System.out.println(e);
         }
     }
-
-    private static Employees readEmployees(String location) throws IOException {
-        String jsonString = new String(Files.readAllBytes(Paths.get(location)));
-        Gson json = new GsonBuilder().create();
-        return json.fromJson(jsonString, Employees.class);
-    }
-
-    private static void exampleOfReadingFileToString() throws IOException {
-        final String fileName = "manifest.mf";
-
-        // How to read file into String before Java 7
-        InputStream is = new FileInputStream(fileName);
-        BufferedReader buf = new BufferedReader(new InputStreamReader(is));
-        String line = buf.readLine();
-        StringBuilder sb = new StringBuilder();
-        while (line != null) {
-            sb.append(line).append("\n");
-            line = buf.readLine();
-        }
-        String fileAsString = sb.toString();
-        System.out.println("Contents (before Java 7) : " + fileAsString);
-
-        // Reading file into String in one line in JDK 7
-        String contents = new String(Files.readAllBytes(Paths.get(fileName)));
-        System.out.println("Contents (Java 7) : " + contents);
-
-        // Reading file into String using proper character encoding
-        String fileString = new String(Files.readAllBytes(Paths.get(fileName)),
-                StandardCharsets.UTF_8);
-        System.out.println("Contents (Java 7 with character encoding ) : " + fileString);
-
-        // It's even easier in Java 8
-        Files.lines(Paths.get(fileName), StandardCharsets.UTF_8).forEach(System.out::println);
-    }
-
 }
--- lect03/JEE_DataLayer/src/main/java/ru/otus/dpopkov/xmljsonhw/servlets/MarshalXMLServlet.java
+++ lect03/JEE_DataLayer/src/main/java/ru/otus/dpopkov/xmljsonhw/servlets/MarshalXMLServlet.java
@@ -7,7 +7,6 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import ru.otus.dpopkov.jdbc.model.Employee;
 import ru.otus.dpopkov.jdbc.model.Employees;
-import ru.otus.dpopkov.jdbc.model.Position;
 import ru.otus.dpopkov.jdbc.util.JPASessionUtil;
 import ru.otus.dpopkov.resource.XmlBean;
 
@@ -20,19 +19,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.*;
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -131,41 +124,12 @@ public class MarshalXMLServlet extends HttpServlet {
     }
 
     private void writeXml(HttpServletResponse resp) throws ServletException, IOException {
-        //        List<Employee> list = prepareEmployees();
         List<Employee> list = getEmployees();
         Employees employees = new Employees(list);
         xmlBean.process(employees);
-//        process(employees, MarshalXMLServlet.class.)
 
         PrintWriter writer = resp.getWriter();
         writer.println("xml path = " + xmlBean.getXmlPath());
-    }
-
-    //todo: remove not needed method
-    /* This method used for testing */
-    private List<Employee> prepareEmployees() {
-        List<Employee> list = new ArrayList<>();
-        Employee employee;
-        Position hr = new Position(10L, "HR");
-        employee = new Employee(1L, "Petrov Petr", BigDecimal.valueOf(100000L));
-        employee.setPosition(hr);
-        list.add(employee);
-        employee = new Employee(2L, "Nititin Nikita", BigDecimal.valueOf(200000L));
-        employee.setPosition(hr);
-        list.add(employee);
-        return list;
-    }
-
-    //todo: remove not needed method
-    private void process(Object persistObject, OutputStream outputStream) {
-        try {
-            JAXBContext jaxbContext = JAXBContext.newInstance(persistObject.getClass());
-            Marshaller marshaller = jaxbContext.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            marshaller.marshal(persistObject, outputStream);
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
     }
 
     private List<Employee> getEmployees() throws ServletException {
