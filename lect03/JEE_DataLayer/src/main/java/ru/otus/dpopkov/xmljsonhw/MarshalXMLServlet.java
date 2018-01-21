7c1dd5a3034bf9a75a9c5ed5cc56d05ac81116db
dpopkov <pkvdenis@gmail.com>
2018-01-21 17:33:15 +0300

    lect04: HW03: Use XPath in MarshalXMLServlet

--- lect03/JEE_DataLayer/src/main/java/ru/otus/dpopkov/xmljsonhw/MarshalXMLServlet.java
+++ lect03/JEE_DataLayer/src/main/java/ru/otus/dpopkov/xmljsonhw/MarshalXMLServlet.java
@@ -1,5 +1,10 @@
 package ru.otus.dpopkov.xmljsonhw;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 import ru.otus.dpopkov.jdbc.model.Employee;
 import ru.otus.dpopkov.jdbc.model.Employees;
 import ru.otus.dpopkov.jdbc.model.Position;
@@ -18,12 +23,18 @@ import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @WebServlet(
         name = "marshalXmlServlet",
@@ -35,7 +46,92 @@ public class MarshalXMLServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        List<Employee> list = prepareEmployees();
+        String action = req.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "write":
+                writeXml(resp);
+                break;
+            case "find":
+                findEmployees(resp);
+                break;
+            default:
+                resp.getWriter().println("Allowed values for action parameter: write, find");
+                break;
+        }
+    }
+
+    private void findEmployees(HttpServletResponse resp) throws ServletException {
+        String location = xmlBean.getXmlPath().toString();
+
+        try {
+            Document document = getDocument(location);
+
+            Element root = document.getDocumentElement();
+            System.out.println(root.getNodeName() + ":");
+
+            XPathFactory xPathFactory = XPathFactory.newInstance();
+            XPath xPath = xPathFactory.newXPath();
+            XPathExpression xPathExpression;
+            Object result;
+
+            xPathExpression = xPath.compile("sum(//employee/salary)");
+            result = xPathExpression.evaluate(document, XPathConstants.NUMBER);
+            double sum = (double) result;
+
+            xPathExpression = xPath.compile("count(//employee/salary)");
+            result = xPathExpression.evaluate(document, XPathConstants.NUMBER);
+            double count = (double) result;
+
+            double averageSalary = sum / count;
+            String averageSalaryStr = String.format(Locale.ENGLISH, "%.2f", averageSalary);
+
+            String expression = "//employee[salary>" + averageSalaryStr + "]";
+            xPathExpression = xPath.compile(expression);
+            result = xPathExpression.evaluate(document, XPathConstants.NODESET);
+            NodeList nodeList = (NodeList) result;
+
+            displayEmployees(nodeList, resp);
+        } catch (ParserConfigurationException | IOException | SAXException | XPathExpressionException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    private void displayEmployees(NodeList nodeList, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.append("<html><body><table>\r\n");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node;
+                writer.append("<tr>");
+                displayElement(writer, element, "id");
+                displayElement(writer, element, "fullName");
+                displayElement(writer, element, "login");
+                displayElement(writer, element, "salary");
+                writer.append("</tr>\r\n");
+            }
+        }
+        writer.append("</table></body></html>\r\n");
+    }
+
+    private static void displayElement(PrintWriter writer, Element element, String tagName) {
+        writer.println("<td>" + element.getElementsByTagName(tagName).item(0).getTextContent() + "</td>");
+    }
+
+    private Document getDocument(String location) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document document = builder.parse(new File(location));
+        document.getDocumentElement().normalize();
+        return document;
+    }
+
+    private void writeXml(HttpServletResponse resp) throws ServletException, IOException {
+        //        List<Employee> list = prepareEmployees();
         List<Employee> list = getEmployees();
         Employees employees = new Employees(list);
         xmlBean.process(employees);
